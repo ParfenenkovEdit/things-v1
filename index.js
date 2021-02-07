5021df558ed459e4fa6378b4022e0cf8e10c2c39
ParfenenkovEdit <parfenenkovedit@gmail.com>
2021-02-07 21:52:43 +0300

    1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 
 app.use(bodyParser.json({ type: 'application/json' }));
